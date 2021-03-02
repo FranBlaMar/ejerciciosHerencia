ca90a2e1a74d1f4fcb429fc8d5c1d2ef0010b550
Fran <franbl98@gmail.com>
2021-03-02 10:12:42 +0100

    Try Catch en main de barco

--- src/Barco/mainBarco.java
+++ src/Barco/mainBarco.java
@@ -5,11 +5,17 @@ public class mainBarco {
 
 	public static void main(String[] args) throws Exception{
 		Deportivo d1 = new Deportivo ("1234AE", 100, 2002, 200);
-		Alquiler a1 = new Alquiler ("Fran","77823435D", LocalDate.parse("2021-10-12"),LocalDate.parse("2022-10-22"), 23, d1);
+		System.out.println(d1.precioBarco());
+		
+		try {
+		Alquiler a1 = new Alquiler ("Fran","77823435D", LocalDate.parse("2021-10-12"),LocalDate.parse("2021-10-22"), 23, d1);
 		System.out.println(d1);
 		System.out.println(a1);
-		System.out.println(d1.precioBarco());
 		System.out.println(a1.calcularPrecioAlquiler());
+		}catch(AlquilerException E) {
+			System.out.println(E);
+		}
+		
 	}
 
 }
